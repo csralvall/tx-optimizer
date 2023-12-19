4d3037168479195a52d96867615cd1bb28ce9965
csralvall <46329881+csralvall@users.noreply.github.com>
2023-12-18 22:16:44 -0300

    doc(datatypes): add docstrings to utxo module

--- src/datatypes/utxo.py
+++ src/datatypes/utxo.py
@@ -1,3 +1,10 @@
+"""A module with the needed data to create a simplified UTxO model.
+
+This module includes:
+    - OutputType (class): the type of the script inside the UTxO.
+    - TypeMetadata (class): metadata associated with the OutputType.
+    - UTxO (class): a simplified Unspent Transaction Output model.
+"""
 from __future__ import annotations
 
 from dataclasses import dataclass, field
@@ -12,26 +19,54 @@ LOGGER = structlog.stdlib.get_logger(__name__)
 
 
 class TypeMetadata(NamedTuple):
+    """Weight data associated to each UTxO to compute its fees.
+
+    Attributes:
+        input_weight: the weight of the UTxO when included as a transaction
+            input (mainly witness data).
+        output_weight: the weight of the UTxO when produced by a transaction.
+    """
+
     input_weight: int
     output_weight: int
 
 
 class OutputType(Enum):
+    """Representation of the types of ScriptPubKeys available."""
+
+    #: Pay to Witness Public Key Hash
     P2WPKH = TypeMetadata(input_weight=68, output_weight=31)
 
 
 @dataclass
 class UTxO:
+    """A simplification of an Unspent Transaction Output.
+
+    Attributes:
+        output_type: the type of the ScriptPubKey of the UTxO.
+        amount: the amount of satoshis this UTxO has blocked in it.
+        wallet_id: a unique field to identify the UTxO inside the simulation
+            wallet.
+    """
+
     output_type: OutputType
     amount: int
     wallet_id: int = field(init=False, default=-1)
 
     def input_fee(self, fee_rate: FeeRate) -> int:
-        "Returns fee associated to the expenditure of this utxo"
+        """The fee to spend the UTxO as input at the specified fee rate.
+
+        Args:
+            fee_rate: the fee rate at which the UTxO is going to be spend.
+        """
         input_weight: int = self.output_type.value.input_weight
         return fee_rate.fee(input_weight)
 
     def output_fee(self, fee_rate: FeeRate) -> int:
-        "Returns fee associated to the creation of this utxo"
+        """The fee to spend the UTxO as output at the specified fee rate.
+
+        Args:
+            fee_rate: the fee rate at which the UTxO is going to be created.
+        """
         output_weight: int = self.output_type.value.output_weight
         return fee_rate.fee(output_weight)
